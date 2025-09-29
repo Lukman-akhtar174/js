9538b8b7057cbf24b3a4d62a8cc3909f215b5578
Lukman-akhtar174 <lukmanakhtar105@gmail.com>
2025-09-29 10:42:45 +0000

    var change

--- 01_basic/01_Variables.js
+++ 01_basic/01_Variables.js
@@ -3,5 +3,7 @@ var accountName = "lukman";
 let accountEmail = 'lk@google.com';
 
 accountCity = 'Lucknow';
+/* Prefer to not use var because of its function scope
+   Use let and const because of their block scope */
 
 console.table([accountId, accountName, accountEmail, accountCity]);
