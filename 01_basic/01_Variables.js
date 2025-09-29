18fbefc75d8c5be423a52620d3452b1bfefeebf9
Lukman-akhtar174 <lukmanakhtar105@gmail.com>
2025-09-29 10:32:38 +0000

    first code

---
+++ 01_basic/01_Variables.js
@@ -0,0 +1,7 @@
+const accountId = 12345;
+var accountName = "lukman";
+let accountEmail = 'lk@google.com';
+
+accountCity = 'Lucknow';
+
+console.table([accountId, accountName, accountEmail, accountCity]);
