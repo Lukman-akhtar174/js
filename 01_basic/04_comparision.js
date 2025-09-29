95a6204e17f8dcd1713035d634cbc5c226496468
Lukman-akhtar174 <lukmanakhtar105@gmail.com>
2025-09-29 13:31:58 +0000

    comparision

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,28 @@
+// console.log(2 > 3);//false
+// console.log(2 < 3);//true
+
+// console.log(2 >= 3);//false
+// console.log(3 >= 3);//true
+
+// console.log(2 <= 3);//true
+// console.log(3 <= 3);//true
+
+// console.log(2 == 2);//true
+// console.log(2 == 3);//false
+
+// console.log(2 != 2);//false
+// console.log(2 != 3);//true
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null > 0);
+console.log(null <= 0);
+console.log(null < 0);
+
+
+// === (same datatype)
+console.log(2 === 2);//true
+console.log(2 === "2");//false
