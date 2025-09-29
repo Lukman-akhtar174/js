748e4cacc1c1e5eeca4b8beb84547d029249f2c5
Lukman-akhtar174 <lukmanakhtar105@gmail.com>
2025-09-29 12:48:38 +0000

    data type

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict";//treat all js files as newer version
+
+// alert("Hello World!");//we use nodejs, not browser, so alert() won't work
+
+console.log(3 
+    +
+     3);// code readablitiy should be high
+
+console.log("LUKMAN");
+
+let name = "Lukman";//string
+let age = 20;//number
+let isApproved = false;//boolean
+let latName;//undefined
+let selectedColor = null;//null
+
+console.log(typeof null);//object
+console.log(typeof lastName);//undefined
