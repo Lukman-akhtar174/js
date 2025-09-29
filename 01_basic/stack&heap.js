c537137744bd568d47636fc96721662c75a66838
Lukman-akhtar174 <lukmanakhtar105@gmail.com>
2025-09-29 14:09:04 +0000

    stack and heap

---
+++ 01_basic/stack&heap.js
@@ -0,0 +1,17 @@
+let myName = "Lukman";
+
+let anotherName = myName;
+
+anotherName = "Kashif";
+
+console.log(myName);
+console.log(anotherName);
+
+let personOne = { name: "Lukman", age: 20 };
+
+let personTwo = personOne;
+
+personTwo.name = "Kashif";
+
+console.log(personOne.name);
+console.log(personTwo.name);
